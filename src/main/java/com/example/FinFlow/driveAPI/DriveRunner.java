217cfc71188a2bac4ca3deed205f352cc765a81d
CatVshyx <dmitroshapiro@gmail.com>
2023-03-31 19:01:29 +0300

    deleted unnecessary things in drive runner

--- src/main/java/com/example/FinFlow/driveAPI/DriveRunner.java
+++ src/main/java/com/example/FinFlow/driveAPI/DriveRunner.java
@@ -19,33 +19,30 @@ import java.util.List;
 public class DriveRunner {
     private static final String APPLICATION_NAME = "Google Drive API Java Quickstart";
     private static final GsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-    private static final java.io.File CREDENTIALS_FOLDER = new java.io.File(System.getProperty("user.dir"));
-    private static final String CLIENT_SECRET_FILE_NAME = "credentials.json";
+//    private static final java.io.File CREDENTIALS_FOLDER = new java.io.File(System.getProperty("user.dir"));
+//    private static final String CLIENT_SECRET_FILE_NAME = "credentials.json";
     private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE);
     private static final String credentials = "{\"installed\":{\"client_id\":\"500209315233-oh5bgvc9m4ijs8lvhcorfib0tkvpmi4i.apps.googleusercontent.com\",\"project_id\":\"datasource-381016\",\"auth_uri\":\"https://accounts.google.com/o/oauth2/auth\",\"token_uri\":\"https://oauth2.googleapis.com/token\",\"auth_provider_x509_cert_url\":\"https://www.googleapis.com/oauth2/v1/certs\",\"client_secret\":\"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\",\"redirect_uris\":[\"http://localhost\"]}}";
     private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
-        System.out.println("CREDENTIALS INITIALIZATION");
+        System.out.println("CREDENTIALS INITIALIZATION v2");
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new StringReader(credentials));
 
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
-                clientSecrets, SCOPES).setDataStoreFactory(new FileDataStoreFactory(CREDENTIALS_FOLDER))
-                .setAccessType("offline").build();
+                clientSecrets, SCOPES).setAccessType("offline").build();
 
         return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
     }
     public static Drive getDrive() {
-        if (!CREDENTIALS_FOLDER.exists()) {
-            CREDENTIALS_FOLDER.mkdirs();
-            System.out.println("Created Folder: " + CREDENTIALS_FOLDER.getAbsolutePath());
-            System.out.println("Copy file " + CLIENT_SECRET_FILE_NAME + " into folder above.. and rerun this class!!");
-            return null;
-        }
+//        if (!CREDENTIALS_FOLDER.exists()) {
+//            CREDENTIALS_FOLDER.mkdirs();
+//            System.out.println("Created Folder: " + CREDENTIALS_FOLDER.getAbsolutePath());
+//            System.out.println("Copy file " + CLIENT_SECRET_FILE_NAME + " into folder above.. and rerun this class!!");
+//            return null;
+//        }
         try{
             final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
             Credential credential = getCredentials(HTTP_TRANSPORT);
-
-            return new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential) //
-                    .setApplicationName(APPLICATION_NAME).build();
+            return new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName(APPLICATION_NAME).build();
         }catch (IOException | GeneralSecurityException e){
             e.printStackTrace();
         }
